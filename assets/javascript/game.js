ee8c896145e6c6a8aa051b1cc822512cea32fc70
Jonbonbroski <jonbonbroski@gmail.com>
2019-05-31 21:58:38 -0700

    Done with the @#$%ing buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var characterImages = ["assets/images/ajani.jpg", "assets/images/chandra.jpg", "assets/images/liliana.jpg"];
-
+var characterPics = ["assets/images/ajani.jpg", "assets/images/chandra.jpg", "assets/images/liliana.jpg"];
 var ajani = {Name: "Ajani",
              Power: 5,
              Health: 150
@@ -13,13 +13,31 @@ var liliana = { Name: "Liliana",
                 Health:100,
 };
 
+//var characters and buttonVal help link selection to object from radio buttons. 
 var characters = [ajani, chandra, liliana];
+var buttonVal = ["Ajani","Chandra","Liliana"];
 var playerChoice;
 var enemyChoice;
 
+//variables for slected characters and their pictures. 
+var playerCharacter;
+var playerImage;
+var enemyCharacter;
+var enemyImage;
+
+
+// variable to let me hide beaten characters from selection page;
+var killedCharacters= [];
+
+var playerHealth;
+var playerPower;
+var enemyHealth;
+var enemyPower;
 //Loads character images,name and stats in html and links classes.
 
 $("#character-selection").ready(function(){
+    $("#battle-screen").hide();
+    $("#fight-button").hide();
 
     for(var i = 0; i < characterImages.length; i++){
 
@@ -28,10 +46,10 @@ $("#character-selection").ready(function(){
         var charPower = characters[i].Power;
         var charHealth = characters[i].Health;
         var charChoice = characters[i];
-
+        
         //html layout consists of label/button/image and p tag
         //messy but works
-        characterImages[i] = $("<label class=\"btn btn-outline-dark\"><input type=\"radio\" value="+ charName +" + name=\"options\" class=\"characterButtons\" autocomplete=\"off\"><img class=\"characterImages\" src =" + characterImages[i] + ">" + 
+        characterImages[i] = $("<label class=\"btn btn-outline-dark\"><input type=\"radio\" value="+ buttonVal[i] +" + name=\"options\" class=\"characterButtons\" autocomplete=\"off\"><img class=\"characterImages\" src =" + characterImages[i] + ">" + 
         "<p>"+charName +"</p>" + "<p>Power:" + charPower +"</p>" + "<p>Health:" + charHealth +"</p>"+ "</input></label>");
         $("#character-select").append(characterImages[i]);
         $(characterImages[i]).addClass("select-character col-sm-2");
@@ -63,13 +81,68 @@ $("#accept-btn").on("click", function(){
 });
 
 
-//
-$(document).ready(function(){
+//Function that starts the battle.
+$("#start-btn").on("click", function(){
     if(enemyChoice != undefined){
-        $("#accept-btn").on("click", function(){
+//loop that checks values from buttons and
+//then links that to object for characters.
+    for(i = 0; i < buttonVal.length; i++){
+        if(playerCharacter === undefined){
+           if(playerChoice == buttonVal[i]){
+               playerCharacter = characters[i];
+               playerImage = $("<img class=\"battle-pic\" src="+characterPics[i]+"><p>"+buttonVal[i]+"</p></img><div class=\"progress\"><div class=\"progress-bar progress-bar-striped bg-danger\" id=\"player-bar\" role=\"progressbar\" style=\"width: 100%\" aria-valuenow="+ playerCharacter.Health +" aria-valuemin=\"0\" aria-valuemax="+ playerCharacter.Health +"></div></div>");
+               console.log(playerCharacter);
+           }};
+        if(enemyCharacter === undefined){   
+            if(enemyChoice == buttonVal[i]){
+                enemyCharacter = characters[i];
+                enemyImage = $("<img class=\"battle-pic\" src="+characterPics[i]+"><p>"+buttonVal[i]+"</p></img><div class=\"progress\"><div class=\"progress-bar progress-bar-striped bg-danger\" id=\"enemy-bar\" role=\"progressbar\" style=\"width: 100%\" aria-valuenow="+ enemyCharacter.Health +" aria-valuemin=\"0\" aria-valuemax="+ enemyCharacter.Health +"></div></div>");
+                console.log(enemyCharacter);
+        };
+    }};
 
-            
-})};
+//Hides HTML for character selection and makes battle screen. 
+    
+        $("#character-select").hide();
+        $("#select-buttons").hide();
+        $("#battle-screen").show();
+        $("#player-spot").append(playerImage);
+        $("#enemy-spot").append(enemyImage);
+        $("#fight-button").show();
+
+//created vars here so character can level up 
+//I know instructions show health does not return to full
+//but I'm a rebel... "RAGL FRAGL"
+    playerHealth = playerCharacter.Health;
+    playerPower = playerCharacter.Power;
+    enemyHealth = enemyCharacter.Health;
+    enemyPower = enemyCharacter.Power;
+
+
+//onClick fight button that does damage and reflects on progress bars.
+
+    $("#fight-button").on("click", function(){
+        var damage = playerHealth - enemyPower;
+        playerHealth = damage;
+        $("#player-bar").attr('aria-valuenow', playerHealth).css('width', playerHealth);
+        var attack = enemyHealth - playerPower;
+        enemyHealth = attack;
+        $("#enemy-bar").attr('aria-valuenow', enemyHealth).css('width', enemyHealth);
+        console.log(damage);
+        console.log(attack);
+        
+
+
+    });
+
+
+
+
+
+
+}else{
+    alert("You must choose an enemy before starting.")
+};
 
 });
 
